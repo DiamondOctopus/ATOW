eb551786190abea45189f9a3afac121e18421f9c
DiamondOctopus <neilb13@gmail.com>
2025-09-24 18:02:45 -0400

    test for v2 changes

--- sheets/item-sheet.mjs
+++ sheets/item-sheet.mjs
@@ -1,4 +1,4 @@
-const { ItemSheetV2 } = foundry.applications.sheets
+const { ItemSheetV2 } = foundry.applications.sheets.ItemSheetV2
 
 
 export class ATOWItemSheet extends HandlebarsApplicationMixin(ItemSheetV2) {
