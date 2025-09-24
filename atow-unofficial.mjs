02f03fb9f23f4f2bd6a4ebc7d419439883f05c17
DiamondOctopus <neilb13@gmail.com>
2025-09-24 17:54:07 -0400

    ah, v2, missed a spot

--- atow-unofficial.mjs
+++ atow-unofficial.mjs
@@ -12,12 +12,12 @@ Hooks.once("init", () => {
     CONFIG.Item.documentClass = ATOWItem;
 
     //sheets
-    foundry.documents.collections.Actors.unregisterSheet('core', ActorSheet);
+    foundry.documents.collections.Actors.unregisterSheet('core', foundry.applications.sheets.ActorSheetV2);
     foundry.documents.collections.Actors.registerSheet('atow-unofficial', ATOWActorSheet,{
         makeDefault: true,
         label: 'ATOW Actor Sheet',
     });
-    foundry.documents.collections.Items.unregisterSheet('core', ItemSheet);
+    foundry.documents.collections.Items.unregisterSheet('core', foundry.applications.sheets.ItemSheetV2);
     foundry.documents.collections.Items.registerSheet('atow-unofficial', ATOWItemSheet,{
         makeDefault: true,
         label: 'ATOW Item Sheet',
