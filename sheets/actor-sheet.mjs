1510c24eb97c9970ac8269dc1f7a39b832972891
DiamondOctopus <neilb13@gmail.com>
2025-09-27 14:41:35 -0400

    todo figure out why inputs on actor sheet dont save their value. fuck.

--- sheets/actor-sheet.mjs
+++ sheets/actor-sheet.mjs
@@ -10,6 +10,7 @@ export class ATOWActorSheet extends HandlebarsApplicationMixin(ActorSheetV2) {
     // - Item sorting within the same actor via _onSortItem
     // - Document drop handling with delegation to _onDropItem, _onDropActiveEffect, etc.
     static DEFAULT_OPTIONS = {
+        classes: ['atow', 'sheet', 'actor'],
         position: {
             width: 600,
             height: 600
@@ -40,10 +41,58 @@ export class ATOWActorSheet extends HandlebarsApplicationMixin(ActorSheetV2) {
             template: 'systems/atow-unofficial/templates/actor-sheet.hbs'
         }
     }
-//    async _prepareContext() {
-//        const context = await super.perpareContext()
-//        context.system = this.actor.system
-//        context.flags = this.actor.flags
-//        return context
-//    }
+    async _prepareContext() {
+        const context = await super._prepareContext()
+
+        context.system = this.actor.system;
+        context.flags = this.actor.flags;
+
+        if(this.actor.type === 'character') {
+            this._prepareItems(context);
+            this._prepareCharacterData(context);
+        }
+
+        if(this.actor.type === 'npc' || this.actor.type === 'enemy') {
+            this._prepareItems(context);
+        }
+
+        context.enrichedBiography = await foundry.applications.ux.TextEditor.implementation.enrichHTML(
+            this.actor.system.biography,
+            {
+                secrets: this.document.isOwner,
+                relativeTo: this.actor,
+            }
+        );
+
+       return context
+    }
+
+    _prepareCharacterData(context) {
+
+    }
+
+    _prepareItems(context) {
+        context.items = this.actor.items;
+        const gear = [];
+        const weps = [];
+        const armors = [];
+
+
+        for (let i of context.items) {
+            i.img - i.img || Item.DEFAULT_ICON;
+            if (i.type === 'equipment') {
+                gear.push(i);
+            } else if (i.type === 'weapon') {
+                weps.push(i);
+            } else if (i.type === 'armor') {
+                armors.push(i);
+            }
+        }
+
+        context.gear = gear;
+        context.weps = weps;
+        context.armors = armors;
+    }
+
+
 }
